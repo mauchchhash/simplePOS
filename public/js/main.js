e9426146839caee346b5b84c25c0d46b284092d3
Mahim Anjum Uchchhwas <mauchchhash@gmail.com>
2019-09-07 03:35:03 +0600

    Added some vue layers in welcome page

--- public/js/main.js
+++ public/js/main.js
@@ -5,3 +5,24 @@ $(document).ready(function() {
   });
 });
 */
+
+var pos = new Vue({
+  el: '#pos-row',
+  data: {
+    activeChoice: 'category'
+  },
+  computed:{
+    setCategory(){
+      this.activeChoice = 'category'
+    },
+    setBeverage(){
+      this.activeChoice = 'beverage'
+    },
+    setFood(){
+      this.activeChoice = 'food'
+    },
+    setOther(){
+      this.activeChoice = 'other'
+    }
+  }
+})
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,33 +2,128 @@
 
 @section('content')
 
-  <div class="card-deck">
-    <div class="card bg-primary">
-      <a href="#">
+  <div class="row" id="pos-row">
+    <div class="card-deck col-md-8" v-if="activeChoice == 'category'">
+      <div class="card bg-primary col-md-9">
+        <!-- <a href="#"> -->
         <div class="card-body text-center">
           <br>
-          <h1 class="card-text text-white">Beverages</h1>
+          <h2 class="card-text text-white">Beverages</h2>
           <br>
         </div>
-      </a>
-    </div>
-    <div class="card bg-warning">
-      <a href="#">
+        <!-- </a> -->
+      </div>
+      <div class="card bg-warning">
+        <!-- <a href="#"> -->
         <div class="card-body text-center">
           <br>
           <h1 class="card-text text-dark">Foods</h1>
           <br>
         </div>
-      </a>
-    </div>
-    <div class="card bg-success">
-      <a href="#">
+        <!-- </a> -->
+      </div>
+      <div class="card bg-success">
+        <!-- <a href="#"> -->
         <div class="card-body text-center">
           <br>
           <h1 class="card-text text-white">Others</h1>
           <br>
         </div>
-      </a>
+        <!-- </a> -->
+      </div>
+    </div>
+
+    <div class="card-deck col-md-8" v-if="activeChoice == 'beverage'">
+        <div class="col-md-4 card bg-primary col-md-9">
+          <!-- <a href="#"> -->
+          <div class="card-body text-center">
+            <br>
+            <h1 class="card-text text-white">Sprite</h1>
+            <br>
+          </div>
+          <!-- </a> -->
+        </div>
+        <div class="col-md-4 card bg-primary col-md-9">
+          <!-- <a href="#"> -->
+          <div class="card-body text-center">
+            <br>
+            <h1 class="card-text text-white">Dew</h1>
+            <br>
+          </div>
+          <!-- </a> -->
+        </div>
+        <div class="col-md-4 card bg-primary col-md-9">
+          <!-- <a href="#"> -->
+          <div class="card-body text-center">
+            <br>
+            <h1 class="card-text text-white">Coke</h1>
+            <br>
+          </div>
+          <!-- </a> -->
+        </div>
+    </div>
+
+    <div class="card-deck col-md-8" v-if="activeChoice == 'food'">
+      <div class="card bg-warning col-md-9">
+        <!-- <a href="#"> -->
+        <div class="card-body text-center">
+          <br>
+          <h1 class="card-text text-white">Burger</h1>
+          <br>
+        </div>
+        <!-- </a> -->
+      </div>
+      <div class="card bg-warning col-md-9">
+        <!-- <a href="#"> -->
+        <div class="card-body text-center">
+          <br>
+          <h1 class="card-text text-white">Pizza</h1>
+          <br>
+        </div>
+        <!-- </a> -->
+      </div>
+      <div class="card bg-warning col-md-9">
+        <!-- <a href="#"> -->
+        <div class="card-body text-center">
+          <br>
+          <h1 class="card-text text-white">Hot Dog</h1>
+          <br>
+        </div>
+        <!-- </a> -->
+      </div>
+    </div>
+
+    <div class="card-deck col-md-8" v-if="activeChoice == 'other'">
+      <div class="card bg-success col-md-9">
+        <!-- <a href="#"> -->
+        <div class="card-body text-center">
+          <br>
+          <h1 class="card-text text-white">Water</h1>
+          <br>
+        </div>
+        <!-- </a> -->
+      </div>
+      <div class="card bg-success col-md-9">
+        <!-- <a href="#"> -->
+        <div class="card-body text-center">
+          <br>
+          <h1 class="card-text text-white">Ice Cream</h1>
+          <br>
+        </div>
+        <!-- </a> -->
+      </div>
+      <div class="card bg-success col-md-9">
+        <!-- <a href="#"> -->
+        <div class="card-body text-center">
+          <br>
+          <h1 class="card-text text-white">Others</h1>
+          <br>
+        </div>
+        <!-- </a> -->
+      </div>
+    </div>
+    <div class="col-md-4 bg-info">
+      <h2 class="text-center my-2 text-white">Order List</h2>
     </div>
   </div>
 @endsection
